4d3fc0390f740d3944169658405c62efe441674f
Raíssa Dias <raissablw@gmail.com>
2024-05-29 22:50:37 -0300

    update player level on profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\PlayerLevel;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,8 +17,11 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $player_levels = PlayerLevel::all();
+
         return view('profile.edit', [
             'user' => $request->user(),
+            'player_levels' => $player_levels
         ]);
     }
 
